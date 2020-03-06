a718877b8bdd077461e9578e8d44d6a37db95ce2
georg-un <georg.unterholzner.coding@gmail.com>
2020-03-06 11:12:06 +0100

    [Feature] Improve user-facing error messages

--- src/main/java/at/finance_otter/service/PurchaseService.java
+++ src/main/java/at/finance_otter/service/PurchaseService.java
@@ -32,13 +32,13 @@ public class PurchaseService {
 
     public PurchaseDTO createPurchase(PurchaseDTO purchaseDTO) throws ExposableException {
         if (purchaseDTO.getPurchaseId() == null) {
-            throw new ExposableException("PurchaseId must not be null");
+            throw new ExposableException("Purchase ID must not be null.");
         } else if (purchaseDTO.getBuyerId() == null) {
-            throw new ExposableException("BuyerId must not be null");
+            throw new ExposableException("Buyer ID must not be null.");
         } else if (purchaseDTO.getDate() == null) {
-            throw new ExposableException("Date must not be null");
+            throw new ExposableException("Date must not be null.");
         } else if (this.databaseAdapter.getUser(purchaseDTO.getBuyerId()) == null) {
-            throw new ExposableException("User with id " + purchaseDTO.getBuyerId() + "not found.");
+            throw new ExposableException("Could not find any user this ID.");
         } else {
             // Create debits
             for (DebitDTO debitDTO : purchaseDTO.getDebits()) {
@@ -58,13 +58,13 @@ public class PurchaseService {
 
     public PurchaseDTO updatePurchase(PurchaseDTO purchaseDTO) throws ExposableException {
         if (purchaseDTO.getPurchaseId() == null) {
-            throw new ExposableException("PurchaseId must not be null");
+            throw new ExposableException("Purchase ID must not be null.");
         } else if (purchaseDTO.getBuyerId() == null) {
-            throw new ExposableException("BuyerId must not be null");
+            throw new ExposableException("Buyer ID must not be null.");
         } else if (purchaseDTO.getDate() == null) {
-            throw new ExposableException("Date must not be null");
+            throw new ExposableException("Date must not be null.");
         } else if (this.databaseAdapter.getUser(purchaseDTO.getBuyerId()) == null) {
-            throw new ExposableException("User with id " + purchaseDTO.getBuyerId() + "not found.");
+            throw new ExposableException("Could not find any user this ID.");
         } else {
             // Delete all debits of the purchase
             for (DebitDTO debitDTO : purchaseDTO.getDebits()) {
@@ -89,15 +89,15 @@ public class PurchaseService {
 
     private Debit createDebit(DebitDTO debitDTO) throws ExposableException {
         if (debitDTO.getDebitId() == null) {
-            throw new ExposableException("DebitId must not be null.");
+            throw new ExposableException("Debit ID must not be null.");
         } else if (debitDTO.getDebtorId() == null) {
-            throw new ExposableException("DebtorId must not be null.");
+            throw new ExposableException("Debtor ID must not be null.");
         } else if (debitDTO.getAmount() == null) {
             throw new ExposableException("Amount must not be null.");
         } else if (this.databaseAdapter.getDebit(debitDTO.getDebitId()) != null) {
-            throw new ExposableException("Debit with DebitId " + debitDTO.getDebitId() + " already exists.");
+            throw new ExposableException("This Debit already exists.");
         } else if (this.databaseAdapter.getUser(debitDTO.getDebtorId()) == null) {
-            throw new ExposableException("User with id " + debitDTO.getDebtorId() + " not found.");
+            throw new ExposableException("Could not find any user this ID.");
         } else {
             Debit debit = new Debit();
             debit.setDebitId(debitDTO.getDebitId());
@@ -109,7 +109,7 @@ public class PurchaseService {
 
     private void deleteDebit(String debitId) throws ExposableException {
         if (debitId == null) {
-            throw new ExposableException("DebitId must not be null.");
+            throw new ExposableException("Debit ID must not be null.");
         } else {
             this.databaseAdapter.deleteDebit(debitId);
         }
--- src/main/java/at/finance_otter/service/UserService.java
+++ src/main/java/at/finance_otter/service/UserService.java
@@ -17,11 +17,11 @@ public class UserService {
 
     public UserDTO createUser(UserDTO userDTO, String userId) throws ExposableException {
         if (userDTO.getFirstName() == null) {
-            throw new ExposableException("First name must not be null");
+            throw new ExposableException("First name must not be null.");
         } else if (userId == null) {
-            throw new ExposableException("userId must not be null");
+            throw new ExposableException("User ID must not be null.");
         } else if (getUser(userId) != null) {
-            throw new ExposableException("userId " + userId + " already has a user associated");
+            throw new ExposableException("This user ID already has a user associated.");
         } else {
             User user = new User();
             user.setUserId(userId);
@@ -45,13 +45,13 @@ public class UserService {
     public UserDTO updateUser(UserDTO userDTO) throws ExposableException {
         // TODO: Prevent that one user can update another
         if (userDTO.getUserId() == null) {
-            throw new ExposableException("UserId must not be null");
+            throw new ExposableException("User ID must not be null.");
         } else if (userDTO.getFirstName() == null) {
-            throw new ExposableException("First name must not be null");
+            throw new ExposableException("First name must not be null.");
         } else {
             User user = databaseAdapter.getUser(userDTO.getUserId());
             if (user == null) {
-                throw new ExposableException("User with id " + userDTO.getUserId() + " not found.");
+                throw new ExposableException("Could not find any user this ID.");
             }
             user.setUserId(userDTO.getUserId());
             user.setFirstName(userDTO.getFirstName());
